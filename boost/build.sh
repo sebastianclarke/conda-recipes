61314f50e3fa39a6ffe9b74be6fe9baf9f184ecf
Pete Bunting <petebunting@mac.com>
2015-04-28 04:07:39 -0700

    Update to the latest versions of the software and fixes to build on mac os x

--- boost/build.sh
+++ boost/build.sh
@@ -12,16 +12,33 @@ export BZIP2_INCLUDE=$PREFIX/include
 export BZIP2_LIBPATH=$PREFIX/lib
 
 if [ `uname` == Darwin ]; then
-    export DYLD_LIBRARY_PATH=$PREFIX/lib
+    export DYLD_LIBRARY_PATH="$PREFIX/lib:$PREFIX/lib/python3.4/config-3.4m"
+    ln -s $PREFIX/lib/python3.4/config-3.4m $PREFIX/lib/python3.4/config
+    ln -s $PREFIX/lib/python3.4/config-3.4m/libpython3.4m.a $PREFIX/lib/python3.4/config-3.4m/libpython3.4.a
 else
     export LD_LIBRARY_PATH=$PREFIX/lib
 fi
 
-./bootstrap.sh --prefix="${PREFIX}" \
- --with-python-version=3.4 \
- --with-python=$PYTHON \
- --with-python-root=$PREFIX  \
- --with-libraries=all
-./b2 include="$PREFIX/include/python3.4m"
+
+
+if [ `uname` == Darwin ]; then
+    ./bootstrap.sh --prefix="${PREFIX}" \
+ 	--with-python-version=3.4 \
+ 	--with-python=$PREFIX/bin/python3.4m \
+ 	--with-python-root=$PREFIX/lib/python3.4/  \
+ 	--with-libraries=all
+	./b2 include="$PREFIX/include/python3.4m"
+	./b2
+else
+    ./bootstrap.sh --prefix="${PREFIX}" \
+ 	--with-python-version=3.4 \
+ 	--with-python=$PYTHON \
+ 	--with-python-root=$PREFIX  \
+ 	--with-libraries=all
+	./b2 include="$PREFIX/include/python3.4m"
+fi
+
+
+
 ./bjam install
 cp bjam $PREFIX/bin
--- hdf4/build.sh
+++ hdf4/build.sh
@@ -4,10 +4,16 @@ mkdir -vp ${PREFIX}/bin;
 
 ARCH="$(uname 2>/dev/null)"
 
-export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC"
+export CFLAGS="-m64 -pipe -O2 -march=x86-64 -fPIC -I${PREFIX}/include"
 export CXXFLAGS="${CFLAGS}"
 #export CPPFLAGS="-I${PREFIX}/include"
-#export LDFLAGS="-L${PREFIX}/lib"
+
+if [ `uname` == Darwin ]; then
+    export DYLD_LIBRARY_PATH=$PREFIX/lib
+else
+    export LD_LIBRARY_PATH=$PREFIX/lib
+    export LDFLAGS="-L${PREFIX}/lib"
+fi
 
 LinuxInstallation() {
 
@@ -27,9 +33,29 @@ LinuxInstallation() {
     return 0;
 }
 
+MacOSXInstallation() {
+
+    chmod +x configure;
+
+    ./configure \
+        --disable-static \
+        --with-ssl \
+        --with-zlib \
+        --prefix=${PREFIX} || return 1;
+    make || return 1;
+    make install || return 1;
+
+    rm -rf ${PREFIX}/share/hdf4_examples;
+
+    return 0;
+}
+
 case ${ARCH} in
     'Linux')
         LinuxInstallation || exit 1;
+        ;;
+    'Darwin')
+        MacOSXInstallation || exit 1;
         ;;
     *)
         echo -e "Unsupported machine type: ${ARCH}";
