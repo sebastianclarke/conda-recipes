51adb1d5e15351ed53fe291458c1017b2d3e1f9f
Daniel Clewley <daniel.clewley@gmail.com>
2015-06-12 12:01:27 -0700

    Use latest GitHub version for laspy. Also run 2to3 for Python 3.

--- laspy/build.sh
+++ laspy/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-python setup.py install --prefix=$PREFIX
+# Library doesn't support Python3.
+# run 2to3 first.
+if [ $PY3K == "1" ]; then
+    2to3 -w .
+    python setup.py install --prefix=$PREFIX
+else
+    python setup.py install --prefix=$PREFIX
+fi
